12e69df152a04155561a3191a4ec4fd3c4ebf52d
Sergey Popov <popovsv1987@gmail.com>
2022-03-18 14:58:19 +0300

    First iteration

---
+++ src/earthquakemap/LandQuakeMarker.java
@@ -0,0 +1,30 @@
+package earthquakemap;
+
+import de.fhpotsdam.unfolding.data.PointFeature;
+import processing.core.PGraphics;
+
+// Implements a visual marker for land earthquakes on an earthquake map
+
+public class LandQuakeMarker extends EarthquakeMarker {
+	
+	
+	public LandQuakeMarker(PointFeature quake) {
+		super(quake);
+		isOnLand = true;
+	}
+
+
+	@Override
+	public void drawEarthquake(PGraphics pg, float x, float y) {
+		pg.ellipse(x, y, 2*radius, 2*radius);
+		
+	}
+	
+
+	// Get the country the earthquake is in
+	public String getCountry() {
+		return (String) getProperty("country");
+	}
+
+		
+}
